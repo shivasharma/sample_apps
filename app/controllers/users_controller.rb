a01ad26c480e1f85381adbfb00f3821bea1df6ba
Shiva Sharma <ss12@umbc.edu>
2012-03-26 23:49:44 -0400

    Finished user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,10 +5,22 @@ class UsersController < ApplicationController
   	@title = @user.name
   end
   
-  
-  
   def new
-@title = "Sign up"
+  	@user=User.new
+  	@title="Sign up"
   end
+  
+  
+  def create
+  		#raise params[user].inspect
+		@user = User.new(params[:user])
+		if @user.save
+			flash[:success]="Welcome to the Sample App!"
+			redirect_to @user
+		else
+			@title = "Sign up"
+			render 'new'
+		end
+	end
 
 end
